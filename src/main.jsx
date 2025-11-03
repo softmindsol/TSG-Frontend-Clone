d66012c86be46bf13eed1267bbaff096eb7ce233
billy-dev <softmindsol@gmail.com>
2025-11-03 15:46:27 +0500

    removing strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { Toaster } from "sonner";
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  <>
     <Toaster richColors position="top-right" />
     <App />
-  </StrictMode>,
-)
+  </>
+  // </StrictMode>
+);
